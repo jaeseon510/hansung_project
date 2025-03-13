250c4df4e3af0981d34c3226a3158be2c9b1fae1
jaeseon510 <jaeseon510@naver.com>
2025-03-13 22:15:34 +0900

    delete: rebase실수수

--- getSource.js
+++
@@ -1,8 +0,0 @@
-function get_source(documment_body){
-    return documment_body.innerText;
-}
-
-chrome.runtime.sendMessage(null,{
-    action: "getSource",
-    source: documment.body.innerHTML
-});
